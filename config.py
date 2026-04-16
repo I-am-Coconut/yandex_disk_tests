33476dea7e00008e513ef7f1682eb43f68512e15
Aleksandr_Kurakin <kurakin.as@yandex.ru>
2026-04-17 01:40:15 +0300

    Добавлены: генерация имён, пояснения

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os # Импортирует модуль Python os. Используется для доступа к переменным окружения через os.getenv()
 from dotenv import load_dotenv # Позволяет загружать переменные окружения из файла .env (OAUTH_TOKEN)
-from datetime import datetime
+from datetime import datetime # Импортирует класс datetime для работы с датой и временем
 
 # Загружаем переменные окружения из файла .env
 load_dotenv()
@@ -16,17 +16,18 @@ HEADERS = {
     "Authorization": f"OAuth {OAUTH_TOKEN}",  # Заголовок авторизации с OAuth-токеном
     "Content-Type": "application/json"  # Указываем, что отправляем JSON-данные
 }
-
+# Функция, генерирующая имя тестовой папки с префиксом test_folder_ и суффиксом в виде текущего времени
 def generate_test_foldername():
     return f"test_folder_{datetime.now().strftime('%H%M%S')}"
 
-# Имя тестовой папки, которая будет создаваться для тестов
+# Переменная, хранящая имя тестовой папки
 TEST_FOLDER = generate_test_foldername()
 
+# Функция, генерирующая имя тестового файла в формате .txt с префиксом test_file_ и суффиксом в виде текущего времени
 def generate_test_filename():
     return f"test_file_{datetime.now().strftime('%H%M%S')}.txt"
 
-# Имя тестового файла для операций
+# Переменная с именем тестового файла
 TEST_FILE_NAME = generate_test_filename()
 
 # Содержимое тестового файла 
